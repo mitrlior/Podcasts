2979c7debdad8757b6efce77f1fcd5b735c0291c
StavRabinovich <stavrab@gmail.com>
2022-01-04 14:35:48 +0200

    Create AdminAPI.js

---
+++ src/shared/util/AdminAPI.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { dumpLogs } from '../Utls';
+import { Container, Row, Col } from 'react-grid-system';
+
+
+const Adminn = (props) => {
+
+  dumpLogs(props);
+
+  return(
+    <div className="Adminn">
+      <h1>Admin Options</h1>
+      <Container>
+        <Row>
+          <Col sm={4}>
+            <button>Delete All Users</button>
+          </Col>
+          <Col sm={4}>
+            <button>Delete All Instances</button>
+          </Col>
+          <Col sm={4}>
+            <button>Delete All Activities</button>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={4}>
+            <button>Get All Users</button>
+          </Col>
+          <Col sm={4}>
+            <button>Get All Activities</button>
+          </Col>
+        </Row>
+        <hr/>  
+        <Row>
+          <Col sm={12}>
+            <div>
+              RESULTS
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>  
+  )
+} 
+
+export default Adminn;
